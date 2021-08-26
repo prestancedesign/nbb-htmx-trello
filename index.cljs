8b71153ae23dc4c497db81f2779be0aa94ee0cdd
Michael SALIHI <admin@prestance-informatique.fr>
2021-08-26 17:42:18 +0200

    List add feature

--- index.cljs
+++ index.cljs
@@ -1,12 +1,27 @@
 (ns index
   (:require ["express$default" :as express]
+            ["pug" :as pug]
+            ["body-parser$default" :as body-parser]
             [data.lists :refer [lists]]))
 
-(def port 3000)
-
-(-> (express)
-    (.set "view engine" "pug")
-    (.use (.static express "assets"))
-    (.get "/" (fn [_req res]
-                (.render res "index" (clj->js lists))))
-    (.listen port #(println "Listening on port:" port)))
+(let [port 3000]
+  (-> (express)
+      (.set "view engine" "pug")
+      (.use (.static express "assets"))
+      (.use (.json body-parser))
+      (.use (.urlencoded body-parser #js {:extended true}))
+      (.get "/" (fn [_req res]
+                  (.render res "index" (clj->js lists))))
+      (.get "/lists/add" (fn [_req res]
+                           (let [template (.compileFile pug "views/_add-list.pug")
+                                 markup (template)]
+                             (.send res markup))))
+      (.post "/lists" (fn [req res]
+                        (let [name (.. req -body -name)
+                              new-list (update lists :lists conj {:id (inc (count (:lists lists)))
+                                                                  :name name
+                                                                  :cards []})
+                              template (.compileFile pug "views/_board.pug")
+                              markup (template (clj->js new-list))]
+                          (.send res markup))))
+      (.listen port #(println "Listening on port:" port))))
