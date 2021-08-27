613ba525bdabe76bec7203462c95bc8af211266d
Michael SALIHI <admin@prestance-informatique.fr>
2021-08-27 16:34:35 +0200

    Move cards and lists router in same ns due to some Nbb limitations

--- index.cljs
+++ index.cljs
@@ -1,9 +1,8 @@
 (ns index
   (:require ["express$default" :as express]
-            ["pug" :as pug]
             ["body-parser$default" :as body-parser]
             [data.lists :refer [lists]]
-            [routes.lists :refer [lists-router]]))
+            [routes.router :refer [lists-router cards-router]]))
 
 (let [port 3000]
   (-> (express)
@@ -14,26 +13,5 @@
       (.get "/" (fn [_req res]
                   (.render res "index" (clj->js @lists))))
       (.use "/lists" lists-router)
-      ;; Cards routes
-      (.get "/cards/edit/:list_id/:id" (fn [req res]
-                                         (let [list-id (int (.. req -params -list_id))
-                                               id (int (.. req -params -id))
-                                               list (get-in @lists [:lists list-id])
-                                               card (first (filter (comp #{id} :id) (:cards list)))
-                                               template (.compileFile pug "views/_edit-card.pug")
-                                               markup (template (clj->js {:id id
-                                                                          :list list
-                                                                          :card card}))]
-                                           (.send res markup))))
-      (.put "/cards/:list_id/:id" (fn [req res]
-                                    (let [label (.. req -body -label)
-                                          list-id (int (.. req -params -list_id))
-                                          id (int (.. req -params -id))
-                                          _ (swap! lists update-in [:lists list-id] assoc-in [:cards (dec id) :label] label)
-                                          list (get-in @lists [:lists list-id])
-                                          card (first (filter #(= id (:id %)) (:cards list)))
-                                          template (.compileFile pug "views/_card.pug")
-                                          markup (template (clj->js {:list list
-                                                                     :card card}))]
-                                      (.send res markup))))
+      (.use "/cards" cards-router)
       (.listen port #(println "Listening on port:" port))))
---
+++ routes/router.cljs
@@ -0,0 +1,48 @@
+(ns routes.router
+  (:require ["express$Router" :as router]
+            ["pug" :as pug]
+            [data.lists :refer [lists]]))
+
+(def lists-router
+  (-> (router)
+      (.get "/add" (fn [_req res]
+                     (let [template (.compileFile pug "views/_add-list.pug")
+                           markup (template)]
+                       (.send res markup))))
+      (.post "/" (fn [req res]
+                   (let [name (.. req -body -name)
+                         id (inc (count (:lists @lists)))
+                         new-list (swap! lists update :lists conj {id {:id id
+                                                                       :name name
+                                                                       :cards []}})
+                         template (.compileFile pug "views/_board.pug")
+                         markup (template (clj->js new-list))]
+                     (.send res markup))))
+      (.get "/cancel" (fn [_req res]
+                        (let [template (.compileFile pug "views/_new-list.pug")
+                              markup (template)]
+                          (.send res markup))))))
+
+(def cards-router
+  (-> (router)
+      (.get "/edit/:list_id/:id" (fn [req res]
+                                   (let [list-id (int (.. req -params -list_id))
+                                         id (int (.. req -params -id))
+                                         list (get-in @lists [:lists list-id])
+                                         card (first (filter (comp #{id} :id) (:cards list)))
+                                         template (.compileFile pug "views/_edit-card.pug")
+                                         markup (template (clj->js {:id id
+                                                                    :list list
+                                                                    :card card}))]
+                                     (.send res markup))))
+      (.put "/:list_id/:id" (fn [req res]
+                              (let [label (.. req -body -label)
+                                    list-id (int (.. req -params -list_id))
+                                    id (int (.. req -params -id))
+                                    _ (swap! lists update-in [:lists list-id] assoc-in [:cards (dec id) :label] label)
+                                    list (get-in @lists [:lists list-id])
+                                    card (first (filter #(= id (:id %)) (:cards list)))
+                                    template (.compileFile pug "views/_card.pug")
+                                    markup (template (clj->js {:list list
+                                                               :card card}))]
+                                (.send res markup))))))
