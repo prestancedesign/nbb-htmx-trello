a07a955baea9bdb0c9b01f1c64564818c1fe2643
Michael SALIHI <admin@prestance-informatique.fr>
2021-08-27 16:27:55 +0200

    Change data structure to a sorted-map

--- data/lists.cljs
+++ data/lists.cljs
@@ -1,11 +1,11 @@
 (ns data.lists)
 
 (def lists
-  (atom {:lists [{:id 1,
-                  :name "To Do",
-                  :cards [{:id 1, :label "First Card", :list 1}
-                          {:id 2, :label "Second Card", :list 1}]}
-                 {:id 2,
-                  :name "Doing",
-                  :cards [{:id 3, :label "First Card", :list 2}
-                          {:id 4, :label "Second Card", :list 2}]}]}))
+  (atom {:lists (sorted-map 1 {:id 1
+                               :name "To Do"
+                               :cards [{:id 1 :label "First Card" :list 1}
+                                       {:id 2 :label "Second Card" :list 1}]}
+                            2 {:id 2
+                               :name "Doing"
+                               :cards [{:id 3 :label "First Card" :list 2}
+                                       {:id 4 :label "Second Card" :list 2}]})}))
