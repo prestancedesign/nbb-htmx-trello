0982670a6c72086a46fc00a127133539434f8208
Michael Salihi <admin@prestance-informatique.fr>
2022-03-24 12:38:02 +0100

    Remove uuseful body-parser since it's now included on last express version

--- index.cljs
+++ index.cljs
@@ -1,6 +1,5 @@
 (ns index
   (:require ["express$default" :as express]
-            ["body-parser$default" :as body-parser]
             [data.db :refer [db]]
             [routes.lists :refer [lists-router]]
             [routes.cards :refer [cards-router]]))
@@ -9,8 +8,8 @@
   (-> (express)
       (.set "view engine" "pug")
       (.use (express/static "assets"))
-      (.use (body-parser/json))
-      (.use (body-parser/urlencoded #js {:extended true}))
+      (.use (express/json))
+      (.use (express/urlencoded #js {:extended true}))
       (.get "/" (fn [_req res]
                   (.render res "index" (clj->js @db))))
       (.use "/lists" lists-router)
